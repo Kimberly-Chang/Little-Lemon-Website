4fd64d94dad95762278ef284337d11d63313ed13
Kimberly C <kc.KimberlyChang@gmail.com>
2024-02-05 15:13:42 -0500

    Added additional setup
    Edited README.md file by removing extraneous content and changing the first header to the project title.
    
    Updated App.js to call components.
    
    Added files for each component called in App.js.
    
    Added the logo.png file.
    
    Added meta tags to the index.html file.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header>
+            <img src="./img/logo.png" alt="logo" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/" class="active">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reserve">Reservations</a></li>
+                <li><a href="/order">Order Online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
